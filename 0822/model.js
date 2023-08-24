295e058ae04ad0a0ac1e1e36f6513a161cd6cbc5
Mihir Kumar <mihir007kr@gmail.com>
2023-08-24 19:06:03 +0530

    Modal

---
+++ 0822/model.js
@@ -0,0 +1,25 @@
+"use strict";
+const btnDisplayModal=document.querySelector(".display-modal");
+const modal = document.querySelector(".modal");
+const btnCloseModal=document.querySelector(".close-modal");
+
+const showModal= function(){
+    modal.classList.remove("hide")
+    document.querySelector(".blureffect").classList.remove("hide")
+};
+
+const closeModal=function(){
+    modal.classList.add("hide")
+    document.querySelector(".blureffect").classList.add("hide")
+
+};
+
+document.addEventListener("keyup",function(event){
+    if (event.key === "x"){
+        closeModal();
+    }
+});
+btnDisplayModal.addEventListener("click",showModal);
+btnCloseModal.addEventListener("click",closeModal);
+
+
