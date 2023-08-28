7650ad5bb8cef6e120e085f17786ea21b8639212
Mihir Kumar <mihir007kr@gmail.com>
2023-08-28 20:01:53 +0530

    Completed

--- picshare/src/App.js
+++ picshare/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css"
 import TodoList from "./components/todolist";
 import Additem from "./components/additem"
@@ -6,29 +6,32 @@ import Additem from "./components/additem"
 
 const App = () => {
 
-    const listitems = [
-        { itemid: Math.trunc(Math.random() * 100 + 1), itemname: "HTML" },
-        { itemid: Math.trunc(Math.random() * 100 + 1), itemname: "CSS" },
-        { itemid: Math.trunc(Math.random() * 100 + 1), itemname: "BootStrap" },
-    ];
+    const [listitems, setListItems] = useState([
+        // { itemid: Math.trunc(Math.random() * 100 + 1), itemname: "HTML" },
+        // { itemid: Math.trunc(Math.random() * 100 + 1), itemname: "CSS" },
+        // { itemid: Math.trunc(Math.random() * 100 + 1), itemname: "BootStrap" },
+    ]);
     const additemHandler = (Newitem) => {
-        listitems.push(Newitem);
-        console.log(listitems);
+        setListItems((previousList) => {
+         return previousList.concat(Newitem);
+        });
+// listitems.push(Newitem);
+// console.log(listitems);
 
-    }
-    const name = "MERN";
-    return (
-        <div className="section">
-            <div className="text1">
-                <p>One Place That Says It All</p>
-            </div>
-            <div className="container">
-                <h1>{name}'s Todo List</h1>
-                <Additem onAdditem={additemHandler} />
-                <TodoList list={listitems} />
-            </div>
+    };
+const name = "MERN";
+return (
+    <div className="section">
+        <div className="text1">
+            <p>One Place To Remember It All</p>
         </div>
-    )
+        <div className="container">
+            <h1>{name}'s Todo List</h1>
+            <Additem onAdditem={additemHandler} />
+            <TodoList list={listitems} />
+        </div>
+    </div>
+)
 };
 
 export default App;
